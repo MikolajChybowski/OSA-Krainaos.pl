d142a04d70567d0f55da40f0148a7f346b7e5f5d
MikolajChybowski <chybowskimikolaj@gmail.com>
2025-09-26 22:01:07 +0200

    Add HTML minify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import htmlMinifier from 'vite-plugin-html-minifier'
 
 export default defineConfig({
     plugins: [
-        ViteImageOptimizer({
-            png: { quality: 80 },
-            jpeg: { quality: 75 },
-            webp: { quality: 80 },
-            avif: { quality: 70 },
-        }),
+      htmlMinifier({
+        minify: true,
+      }),
+      ViteImageOptimizer({
+          png: { quality: 80 },
+          jpeg: { quality: 75 },
+          webp: { quality: 80 },
+          avif: { quality: 70 },
+      }),
     ],
 });
