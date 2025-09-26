0f098cb7f3bd7559373917ec473b1d8ef3eb9a69
MikolajChybowski <chybowskimikolaj@gmail.com>
2025-09-26 21:34:16 +0200

    Test netlify build

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
 import './styles/style.css'
 
-console.log('Test')
+import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
+export default defineConfig({
+    plugins: [
+        ViteImageOptimizer({
+            png: { quality: 80 },
+            jpeg: { quality: 75 },
+            webp: { quality: 80 },
+            avif: { quality: 70 },
+            svg: {
+                plugins: [
+                    { name: 'sortAttrs' },
+                ],
+            },
+        }),
+    ],
+});
